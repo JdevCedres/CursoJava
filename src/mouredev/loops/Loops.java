4de51461ab3c6347dc684ea44ce0224c2908e2cd
JdevCedres <jdevcedres@gmail.com>
2026-01-03 13:33:44 +0000

    Loops, for

---
+++ src/mouredev/loops/Loops.java
@@ -0,0 +1,21 @@
+package mouredev.loops;
+
+public class Loops {
+    public static void main(String[] args) {
+        // Loops
+
+        // Bucle for por contador
+        for (int i =0; i < 5; i++){
+            System.out.println("Hola Java!");
+        }
+
+        // ejemplo con array
+
+        String[] names = {"Jose", "Manuel", "Elena"};
+
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+    }
+
+}
